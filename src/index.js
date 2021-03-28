55bba44327bd449e94124c16390262dd33242d89
Shakir Khakimov <s.f.khakimov@gmail.com>
2021-03-28 23:46:14 +0300

    dev добавил страницу профиля

--- src/index.js
+++ src/index.js
@@ -1,9 +1,1 @@
-import pug from 'pug'
-import template from '../static/test.pug'
-console.log(pug)
-// console.log(template({ name: 'тест'}))
-console.log(pug.render('div #{name}', { name: 'тест'}))
-// const compiledFunction = pug.compileFile(template);
-//
-// console.log(compiledFunction())
 
